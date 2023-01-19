244d0271a3b67df67277dd6e7a50ddf754179fdb
adebayo <horlorunpemy1990@.gmail.com>
2023-01-19 18:54:38 +0100

    two

---
+++ script.js
@@ -0,0 +1,7 @@
+// typing Animation
+var typed = new Typed(".typing",{
+    strings:[ "", "Web designer","Web developer","Graphic designer","youtuber"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
